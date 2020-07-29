13fbc59f8c459b55011c2a807faffdef1c1a8570
viet duc <ducviet709@gmail.com>
2020-07-29 15:36:38 +0700

    up

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Board from "./components/Board"
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      userName: "Trump",
+      nextPlayer: true,
+      squareList: ['', '', '', '', '', '', '', '', ''],
+      history: [],
+
+    }
+  }
+
+  setParentsState = (obj) => {
+    this.setState(obj)
+  }
+
+  backToPast = (index) => {
+    //1. grab the histroy
+    let past = this.state.history[index];
+    //2. change the value squareList,nextPlayer
+    this.setState({
+      ...this.state,
+      squareList: past.squareList,
+      nextPlayer: past.nextPlayer,
+    });
+  };
+ 
+
+  render() {
+    
+
+    return (
+      <div className="main">
+        <div>
+        <h1>Tic Tac Toe</h1>
+        <h3> User name : {this.state.userName}</h3>
+        {/* <h3>nextPlayer:{this.state.nextPlayer}</h3> */}
+        <ol>History{this.state.history.map((_, index) => {
+          return (<li> <button onClick={() => this.backToPast(index)}> Go To:{index + 1}</button></li>)
+        })}
+        </ol>
+        </div>
+        <Board
+          // squareList={this.state.squareList}
+          setParentsState={this.setParentsState}
+          // nextPlayer={this.state.nextPlayer}
+          // history={this.state.history}
+          {...this.state}
+        />
+      </div>
+    )
+  }
 }
 
-export default App;
---
+++ src/components/Board.js
@@ -0,0 +1,81 @@
+import React, { Component } from 'react'
+import Square from "./Square"
+
+export default class Board extends Component {
+    selectSquare = (id) => {
+        let array = this.props.squareList;
+        let historyArray = this.props.history;
+
+        if (array[id] !== "") {
+            alert("click other square");
+            return;
+        }
+        array[id] = this.props.nextPlayer ? "X" : "O";
+
+        historyArray.push({ squareList: array, nextPlayer: !this.props.nextPlayer })
+        this.props.setParentsState({
+            squareList: array,
+            nextPlayer: !this.props.nextPlayer,
+            history: historyArray,
+        });
+    };
+
+    calculateWinner = (squares) => {
+        const lines = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+        ];
+        for (let i = 0; i < lines.length; i++) {
+            const [a, b, c] = lines[i];
+            if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+                return squares[a];
+            }
+        }
+        return null;
+    }
+    render() {
+        const winner = this.calculateWinner(this.props.squareList);
+        let gameOver = false;
+        if (!winner && this.props.squareList.every((item) => item !== '')) { gameOver = true }
+
+        let status;
+        if (winner) {
+            status = 'Winner: ' + winner;
+        } else if (gameOver) {
+            status = "Game Over"
+        }
+        else {
+            status = 'Next player: ' + (this.props.nextPlayer ? 'X' : 'O');
+        }
+        return (
+            <div>
+                <h3>{status}</h3>
+
+                <div style={{ display: "flex" }}>
+                    <Square id={0} selectSquare={this.selectSquare} value={this.props.squareList[0]} />
+                    <Square id={1} selectSquare={this.selectSquare} value={this.props.squareList[1]} />
+                    <Square id={2} selectSquare={this.selectSquare} value={this.props.squareList[2]} />
+                </div>
+
+                <div style={{ display: "flex" }}  >
+                    <Square id={3} selectSquare={this.selectSquare} value={this.props.squareList[3]} />
+                    <Square id={4} selectSquare={this.selectSquare} value={this.props.squareList[4]} />
+                    <Square id={5} selectSquare={this.selectSquare} value={this.props.squareList[5]} />
+                </div>
+
+                <div style={{ display: "flex" }}   >
+                    <Square id={6} selectSquare={this.selectSquare} value={this.props.squareList[6]} />
+                    <Square id={7} selectSquare={this.selectSquare} value={this.props.squareList[7]} />
+                    <Square id={8} selectSquare={this.selectSquare} value={this.props.squareList[8]} />
+                </div>
+
+            </div>
+        )
+    }
+}
